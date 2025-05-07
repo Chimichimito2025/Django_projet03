0304b8827f012e4579180cd078a6351efc89dc41
unknown <bastardo.manuel@gmail.com>
2025-05-07 17:31:22 -0400

    finish detail

--- store/views.py
+++ store/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from store.models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class ProductListView(ListView):
     model=Product
     template_name="list_products.html"
     context_object_name="products"
+
+class ProductDetailView(DetailView):
+    model=Product
+    template_name="product_detail.html"
+    context_object_name="product"
